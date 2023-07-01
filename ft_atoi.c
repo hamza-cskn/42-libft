27fb5d459365d8954c4a604544844a42d70454d1
hamza <hamzacoskun41@gmail.com>
2023-07-02 00:33:48 +0300

    fix: add blank string and negative number support

--- ft_atoi.c
+++ ft_atoi.c
@@ -9,16 +9,36 @@
 /*   Updated: 2023/06/24 21:56:35 by hcoskun42        ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-int	ft_atoi(char *str)
+int	natural_atoi(const char *str)
 {
-	int	res;
+	unsigned int	res;
 
 	res = 0;
-	while (*str)
+	while (*str >= '0' && *str <= '9')
 	{
 		res = res * 10 + *str - '0';
 		str++;
 	}
 	return (res);
+}
+
+int ft_atoi(const char *str)
+{
+	int sign = 1;
+	if (!str)
+		return 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (!*str)
+		return 0;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+
+	return sign * natural_atoi(str);
 }
