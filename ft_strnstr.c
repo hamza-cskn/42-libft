6b614f5c4603017aa978bba974e29aec1ea17962
hamza <hamzacoskun41@gmail.com>
2023-07-02 00:39:58 +0300

    fix: wrong loop condition.

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,17 +12,17 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *needle, size_t len)
-{
-	size_t	needle_len;
-	size_t	i;
+char *ft_strnstr(const char *str, const char *needle, size_t len) {
+	int needle_len;
+	int i;
 
+	if (!str && !len)
+		return NULL;
 	if (*needle == 0)
 		return ((char *) str);
-	needle_len = ft_strlen(needle);
+	needle_len = (int) ft_strlen(needle);
 	i = 0;
-	while (str[i] && i > (len - needle_len))
-	{
+	while (str[i] && i <= ((int) len - needle_len)) {
 		if (ft_strncmp(str + i, needle, needle_len) == 0)
 			return ((char *) str + i);
 		i++;
