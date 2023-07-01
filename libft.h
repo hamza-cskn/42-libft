314e1fe0e3173841582e0048bc78fe5eac76c18a
hamza <hamzacoskun41@gmail.com>
2023-07-02 00:44:57 +0300

    fix: build error. (i just forgot to cocmmit removing const keyword. please.)

--- libft.h
+++ libft.h
@@ -47,7 +47,7 @@ void	ft_cat(int fd);
 
 char	*ft_strchr(const char *s, int c);
 
-char	*ft_strrchr(const char *s, int c);
+char	*ft_strrchr(char *s, int c);
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
