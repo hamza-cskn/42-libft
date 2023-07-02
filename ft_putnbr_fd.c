a70f352565d0e0b6187ead9e8676779a8fe6ae5a
hamza <hamzacoskun41@gmail.com>
2023-07-02 12:10:59 +0300

    fix: do not use malloc to implement putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -12,7 +12,20 @@
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int nb, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (nb < 0) {
+		ft_putchar_fd('-', fd);
+		if (nb == -2147483648)
+		{
+			ft_putstr_fd("2147483648", fd);
+			return;
+		}
+		nb = -nb;
+	}
+	if (nb >= 10) {
+		ft_putnbr_fd(nb / 10, fd);
+		nb = nb % 10;
+	}
+	ft_putchar_fd(nb + '0',fd);
 }
