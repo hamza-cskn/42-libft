0409a584dd338f1f457ce99de5a9252b53cef3aa
Hamza Coskun <hcoskun@k2m12s07.42kocaeli.com.tr>
2023-07-07 19:07:09 +0300

    fix: unprotected allocation

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hcoskun42 <hcoskun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hcoskun <hcoskun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 20:38:47 by hcoskun42         #+#    #+#             */
-/*   Updated: 2023/06/24 20:41:15 by hcoskun42        ###   ########.tr       */
+/*   Updated: 2023/07/04 15:27:47 by hcoskun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,14 @@ static int	word_length(char const *str, char c)
 	int	i;
 
 	i = 0;
-	while (str[i] != c)
+	while (str[i] && str[i] != c)
 		i++;
 	return (i);
 }
 
-static char	*ft_duplicate(char const *str, int size)
-{
-	int		i;
-	char	*res;
-
-	res = malloc(sizeof(char) * size);
-	if (!res)
-		return (0);
-	i = 0;
-	while (i < size - 1)
-	{
-		res[i] = str[i];
-		i++;
-	}
-	res[i] = 0;
-	return (res);
-}
-
 static int	count_words(char const *str, char c)
 {
-	int	i;
+	int	i; 
 	int	count;
 
 	count = 0;
@@ -63,14 +45,16 @@ static int	count_words(char const *str, char c)
 char	**ft_split(char const *str, char c)
 {
 	char	**result;
-	int		size;
+	int		len;
 	int		word_amount;
 	int		word_index;
 	int		i;
 
 	word_index = 0;
 	word_amount = count_words(str, c);
-	result = malloc(sizeof(char *) * (word_amount + 1));
+	result = (char **) malloc(sizeof(char *) * (word_amount + 1));
+	if (!result)
+		return (NULL);
 	while (*str)
 	{
 		i = 0;
@@ -78,11 +62,11 @@ char	**ft_split(char const *str, char c)
 			i++;
 		if (str[i])
 		{
-			size = word_length(str + i, c) + 1;
-			result[word_index++] = ft_duplicate(str + i, size);
+			len = word_length(str + i, c);
+			result[word_index] = malloc((len + 1) * sizeof(char));
+			ft_strlcpy(result[word_index++], str + i, len + 1);
+			i += len;
 		}
-		while (str[i] && str[i] != c)
-			i++;
 		str += i;
 	}
 	result[word_index] = NULL;
